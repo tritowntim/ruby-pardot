4f539b59af4e60ce7869558276b08ef969fef8dd
Tim Dussinger <tritowntim@gmail.com>
2013-07-10 21:37:00 -0400

    Revert "Revert "Try email query API""
    This reverts commit 059dd4e5a9f429838422834422354b147338c3fc.

--- lib/pardot/objects/emails.rb
+++ lib/pardot/objects/emails.rb
@@ -14,10 +14,21 @@ module Pardot
         
         def read id, params = {} 
           post "/do/read/id/#{id}", params
+        end 
+
+        def query params
+          result = get "/do/query", params, "result"
+          # result["total_results"] = result["total_results"].to_i if result["total_results"]
+          result
         end
-        
+
         protected
-        
+
+        def get path, params = {}, result = "email"
+          response = @client.get "email", path, params
+          result ? response[result] : response
+        end 
+
         def post path, params = {}, result = "email"
           response = @client.post "email", path, params 
           result ? response[result] : response
