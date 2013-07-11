21666b01f2b748cd72962dbb78ff671678b474e3
Tim Dussinger <tritowntim@gmail.com>
2013-07-10 21:39:01 -0400

    Remove all references to email object/API

--- lib/ruby-pardot.rb
+++ lib/ruby-pardot.rb
@@ -6,7 +6,6 @@ require 'pardot/http'
 require 'pardot/error'
 require 'pardot/authentication'
 
-require 'pardot/objects/emails'
 require 'pardot/objects/lists'
 require 'pardot/objects/list_memberships'
 require 'pardot/objects/opportunities'
